898e31d531120a8299db838620b234fcc10cd239
omizsigma <159143701+omizsigma@users.noreply.github.com>
2024-02-06 18:06:52 +0530

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,7 @@
+function fish_greeting
+	set_color yellow
+	echo "In all things have no preferences. "
+end
+starship init fish | source
+./fm6000 -dog -c red -m 8 -g 10 -l 12
+alias locate="plocate"
